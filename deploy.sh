7a82e8f85dac785257f613e944e8a406ec895681
Nicholas Penree <nick@penree.com>
2016-06-06 10:22:02 -0400

    Quick and dirty deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dir=$PWD
+deploy_dir=`mktemp -d`
+msg=$1
+
+if [ "$msg" = "" ]; then
+  msg="Update site"
+fi
+
+make site && \
+  cd $deploy_dir && git clone git@github.com:emvc/emvc.git -b gh-pages && \
+  cd emvc && cp -R $dir/emvc-guide/* ./ && \
+  git add . && git commit -avm "$msg" && git push origin gh-pages
